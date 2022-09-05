7d62c48099bf0ec22be8dbff7b90d5ab9227e377
S.Srikanthan <112878241+IT21821240@users.noreply.github.com>
2022-09-05 14:30:03 +0000

    written the answers

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main() {
-  
+  float sub1,sub2,avg;
+  printf("Enter the mark of subject 01: ");
+  scanf("%f",&sub1);
+  printf("Enter the mark of subject 02: ");
+  scanf("%f",&sub2);
+  avg=(sub1+sub2)/2;
+  printf("Average of the two marks is: %.2f",avg);
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,14 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+  int num,i,sum;
+  printf("Input the number: ");
+  scanf("%d",&num);
+
+  for(i=1;i<=num;i++){
+    sum=sum+i;
+  }
+  printf("sum of numbers from 1 to %d = %d",num,sum);
   return 0;
 }
 
